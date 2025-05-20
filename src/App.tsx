b5abb64201a88fca72490d4c2266b24d8af18edd
DonGobbi <dongobbinshombo@gmail.com>
2025-05-20 19:54:27 +0200

    some corrections

--- src/App.tsx
+++ src/App.tsx
@@ -21,7 +21,7 @@ function App() {
               <Route path="/editor" element={<StoryEditor />} />
             </Routes>
             <AudioControls />
-            {showDebug && <AudioDebugPanel />}
+            
           </Router>
         </StoryProvider>
       </AudioProvider>
